de60ade6845d5f15d8bc10b24a875e6600edef54
Amin <mohammadazimi16@gmail.com>
2022-05-30 17:26:57 +0300

    user login and register

--- controllers/user.js
+++ controllers/user.js
@@ -1,21 +1,20 @@
-const User = require("../models/user");
-const Notification = require("../models/notification");
+const User = require('../models/user');
+const Notification = require('../models/notification');
 const {
   loginValidator,
   registerValidator,
   userRegisterValidator,
   userLoginValidator,
-} = require("../validators/user");
-const bcrypt = require("bcrypt");
-const { pick } = require("../utils/lodash");
-const jwt = require("jsonwebtoken");
-const config = require("config");
-const path = require("path");
-var fs = require("fs");
-const multer = require("multer");
-const NodeCache = require("node-cache");
-const product = require("../models/product");
-
+} = require('../validators/user');
+const bcrypt = require('bcrypt');
+const { pick } = require('../utils/lodash');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const path = require('path');
+var fs = require('fs');
+const multer = require('multer');
+const NodeCache = require('node-cache');
+const product = require('../models/product');
 
 const myCache = new NodeCache({
   stdTTL: 2 * 60 * 60,
@@ -32,19 +31,19 @@ let filename = (req, file, callback) => {
   callback(
     null,
     file.fieldname +
-      "-" +
+      '-' +
       Date.now() +
-      file.originalname.substr(file.originalname.lastIndexOf(".") - 1)
+      file.originalname.substr(file.originalname.lastIndexOf('.') - 1)
   );
 };
 let fileFilter = (req, file, cb) => {
   [
-    "image/jpeg",
-    "image/bmp",
-    "image/png",
-    "image/svg+xml",
-    "image/tiff",
-    "image/webp",
+    'image/jpeg',
+    'image/bmp',
+    'image/png',
+    'image/svg+xml',
+    'image/tiff',
+    'image/webp',
   ].includes(file.mimetype)
     ? cb(null, true)
     : cb(null, false);
@@ -55,26 +54,25 @@ module.exports = {
   register: async (req, res) => {
     let { email } = req.body;
 
-    console.log(req.body)
+    console.log(req.body);
     let { error } = registerValidator(req.body);
     if (error) return res.status(400).send({ message: error.message });
 
     const user = await User.findOne({ email });
-    if (user) return res.status(400).send({message: "user already exist"});
+    if (user) return res.status(400).send({ message: 'user already exist' });
 
     let newUser = new User(
       pick(req.body, [
-        "firstName",
-        "lastName",
-        "email",
-        "countryCode",
-        "phoneNumber",
-        "password",
-        "role",
-        "store",
+        'firstName',
+        'lastName',
+        'email',
+        'countryCode',
+        'phoneNumber',
+        'password',
+        'role',
+        'store',
       ])
     );
-
 
     const salt = await bcrypt.genSalt(10);
     let saltPass = await bcrypt.hash(req.body.password, salt);
@@ -86,17 +84,17 @@ module.exports = {
     const token = newUser.generateAuthToken();
 
     let data = pick(newUser, [
-      "_id",
-      "firstName",
-      "lastName",
-      "email",
-      "countryCode",
-      "phoneNumber",
-      "role",
-      "store",
+      '_id',
+      'firstName',
+      'lastName',
+      'email',
+      'countryCode',
+      'phoneNumber',
+      'role',
+      'store',
     ]);
 
-    res.header("x-auth-token", token).send({ ...data, token });
+    res.header('x-auth-token', token).send({ ...data, token });
   },
   login: async (req, res) => {
     let { email, password } = req.body;
@@ -105,25 +103,29 @@ module.exports = {
     if (error) return res.status(400).send({ message: error.message });
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).send({message: "کاربری با این اطلاعات یافت نشد"});
+    if (!user)
+      return res
+        .status(400)
+        .send({ message: 'کاربری با این اطلاعات یافت نشد' });
 
     const compare = await bcrypt.compare(password, user.password);
 
-    if (!compare) return res.status(400).send({message: "wrong username or password"});
+    if (!compare)
+      return res.status(400).send({ message: 'wrong username or password' });
 
     const token = user.generateAuthToken();
 
     let data = pick(user, [
-      "_id",
-      "firstName",
-      "lastName",
-      "email",
-      "countryCode",
-      "phoneNumber",
-      "role",
+      '_id',
+      'firstName',
+      'lastName',
+      'email',
+      'countryCode',
+      'phoneNumber',
+      'role',
     ]);
 
-    res.header("x-auth-token", token).send({ ...data, token });
+    res.header('x-auth-token', token).send({ ...data, token });
   },
   me: async (req, res) => {
     let id = req.user._id;
@@ -136,15 +138,15 @@ module.exports = {
     let { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (user) return res.status(400).send({message: "user already exist"});
+    if (user) return res.status(400).send({ message: 'user already exist' });
 
     let newUser = new User(
       pick(req.body, [
-        "name",
-        "email",
-        "countryCode",
-        "phoneNumber",
-        "password",
+        'name',
+        'email',
+        'countryCode',
+        'phoneNumber',
+        'password',
       ])
     );
 
@@ -158,22 +160,22 @@ module.exports = {
     const token = newUser.generateAuthToken();
 
     let data = pick(newUser, [
-      "_id",
-      "name",
-      "email",
-      "countryCode",
-      "phoneNumber",
-      "password",
+      '_id',
+      'name',
+      'email',
+      'countryCode',
+      'phoneNumber',
+      'password',
     ]);
 
-    res.header("x-auth-token", token).send({ ...data, token });
+    res.header('x-auth-token', token).send({ ...data, token });
   },
   registerWithPhoneNumber: async (req, res) => {
     let { firstName, lastName, phoneNumber, countryCode, otp } = req.body;
-    if (!otp) return res.status(401).send("missed otp value");
+    if (!otp) return res.status(401).send('missed otp value');
 
     const user = await User.findOne({ phoneNumber });
-    if (user) return res.status(409).send("user already exist");
+    if (user) return res.status(409).send('user already exist');
 
     let newUser = new User({
       firstName,
@@ -187,43 +189,44 @@ module.exports = {
     const token = newUser.generateAuthToken();
 
     let data = pick(newUser, [
-      "_id",
-      "firstName",
-      "lastName",
-      "countryCode",
-      "phoneNumber",
+      '_id',
+      'firstName',
+      'lastName',
+      'countryCode',
+      'phoneNumber',
     ]);
 
     let secret = myCache.get(phoneNumber);
 
-    if (secret != otp) return res.status(400).send("wrong otp");
+    if (secret != otp) return res.status(400).send('wrong otp');
 
     res.send({
-      message: "success",
+      message: 'success',
       user: newUser,
       token,
     });
 
-    res.header("x-auth-token", token).send({ ...data, token });
+    res.header('x-auth-token', token).send({ ...data, token });
   },
   userLogin: async (req, res) => {
     let { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).send({message: "user not exist"});
+    if (!user) return res.status(400).send({ message: 'user not exist' });
 
     const compare = await bcrypt.compare(password, user.password);
 
-    if (!compare) return res.status(400).send({message: "wrong username or password"});
+    if (!compare)
+      return res.status(400).send({ message: 'wrong username or password' });
 
     const token = user.generateAuthToken();
 
-    let data = pick(user, ["_id", "name", "email", "phoneNumber"]);
+    let data = pick(user, ['_id', 'name', 'email', 'phoneNumber']);
 
-    res.header("x-auth-token", token).send({ ...data, token });
+    res.header('x-auth-token', token).send({ ...data, token });
   },
   getAllStores: async (req, res) => {
-    const admins = await User.find({ role: "admin" });
+    const admins = await User.find({ role: 'admin' });
 
     let stores = admins.map((item) => {
       let {
@@ -261,7 +264,7 @@ module.exports = {
     let { id } = req.params;
 
     if (id !== req.user._id) {
-      return res.status(401).send({message: "permission denied"});
+      return res.status(401).send({ message: 'permission denied' });
     }
 
     await User.updateOne(
@@ -272,7 +275,7 @@ module.exports = {
       (err, data) => {
         if (err) return res.status(404).send(`Error: ` + err);
         console.log(data);
-        return res.send({message: "user has been updated"});
+        return res.send({ message: 'user has been updated' });
       }
     );
   },
@@ -286,7 +289,7 @@ module.exports = {
           storage,
           fileFilter,
           limits: { fileSize: uploadFileSize },
-        }).single("avatar")(req, res, (err) => {
+        }).single('avatar')(req, res, (err) => {
           User.findById(id, (err, data) => {
             data.avatar = req.file.filename;
 
@@ -307,14 +310,15 @@ module.exports = {
     let countryCode = req.body.countryCode;
 
     let existUser = await User.findOne({ phoneNumber, countryCode });
-    if (existUser) return res.status(404).send({message: "user already exist"});
+    if (existUser)
+      return res.status(404).send({ message: 'user already exist' });
 
     let code = parseInt(Math.random() * 9000 + 1000);
 
     myCache.set(phoneNumber, code);
     let { accountSid, authToken, from } = twilio;
 
-    const client = require("twilio")(accountSid, authToken);
+    const client = require('twilio')(accountSid, authToken);
 
     client.messages
       .create({
@@ -325,7 +329,7 @@ module.exports = {
       .then((message) => {
         res.send({
           code,
-          status: "sent",
+          status: 'sent',
         });
       })
       .catch(() => {
@@ -338,14 +342,14 @@ module.exports = {
     let countryCode = req.body.countryCode;
 
     let existUser = await User.findOne({ phoneNumber, countryCode });
-    if (!existUser) return res.status(404).send({message: "user not found"});
+    if (!existUser) return res.status(404).send({ message: 'user not found' });
 
     let code = parseInt(Math.random() * 9000 + 1000);
 
     myCache.set(phoneNumber, code);
     let { accountSid, authToken, from } = twilio;
 
-    const client = require("twilio")(accountSid, authToken);
+    const client = require('twilio')(accountSid, authToken);
 
     client.messages
       .create({
@@ -356,7 +360,7 @@ module.exports = {
       .then((message) => {
         res.send({
           code,
-          status: "sent",
+          status: 'sent',
         });
       })
       .catch(() => {
@@ -366,24 +370,23 @@ module.exports = {
   },
   verifyCode: async (req, res) => {
     const { otp } = req.body;
-    if (!otp) return res.status(401).send({message: "missed otp value"});
+    if (!otp) return res.status(401).send({ message: 'missed otp value' });
     const phoneNumber = req.body.phoneNumber;
     const user = await User.findOne({ phoneNumber });
     if (!user)
-      return res
-        .status(404)
-        .send(
-          {message: "user not found, please sign up or filled you country code and phone number"}
-        );
+      return res.status(404).send({
+        message:
+          'user not found, please sign up or filled you country code and phone number',
+      });
 
     const token = user.generateAuthToken();
 
     let secret = myCache.get(phoneNumber);
 
-    if (secret != otp) return res.status(400).send({message: "wrong otp"});
+    if (secret != otp) return res.status(400).send({ message: 'wrong otp' });
 
     res.send({
-      message: "success",
+      message: 'success',
       user,
       token,
     });
@@ -393,7 +396,7 @@ module.exports = {
     let { type, countryCode, phoneNumber, address } = req.body;
 
     let user = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).send({message: "user not found"});
+    if (!user) return res.status(404).send({ message: 'user not found' });
 
     user.address.push({ type, countryCode, phoneNumber, address });
     user = await user.save();
@@ -404,10 +407,11 @@ module.exports = {
     let { type, countryCode, phoneNumber, address } = req.body;
 
     let user = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).send({message: "user not found"});
+    if (!user) return res.status(404).send({ message: 'user not found' });
 
     let addressf = user.address.id(addressId);
-    if (!addressf) return res.status(404).send({message: "address not found"});
+    if (!addressf)
+      return res.status(404).send({ message: 'address not found' });
 
     if (type) addressf.type = type;
     if (countryCode) addressf.countryCode = countryCode;
@@ -415,24 +419,24 @@ module.exports = {
     if (address) addressf.address = address;
 
     await user.save();
-    res.send({message: "address has been updated"});
+    res.send({ message: 'address has been updated' });
   },
   removeAddress: async (req, res) => {
     let { addressId, userId } = req.params;
 
     let user = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).send({message: "user not found"});
+    if (!user) return res.status(404).send({ message: 'user not found' });
 
     user.address.id(addressId).remove();
 
     await user.save();
-    res.send({message: "address has been removed"});
+    res.send({ message: 'address has been removed' });
   },
   userComments: async (req, res) => {
     let { _id } = req.user;
 
     let userComments = await product.find({
-      "comments.user": _id,
+      'comments.user': _id,
     });
 
     let result = userComments.map((el) => {
@@ -446,6 +450,75 @@ module.exports = {
   },
   getAllUsers: async (req, res) => {
     const users = await User.find();
-    res.send(users)
-  }
+    res.send(users);
+  },
+  userNormalRegister: async (req, res) => {
+    let { email } = req.body;
+    console.log(req.body);
+
+    let { error } = registerValidator(req.body);
+    if (error) return res.status(400).send({ message: error.message });
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).send({ message: 'user already exist' });
+
+    let newUser = new User(
+      pick(req.body, [
+        'name',
+        'email',
+        'countryCode',
+        'phoneNumber',
+        'password',
+        'firstName',
+        'lastName',
+      ])
+    );
+
+    const salt = await bcrypt.genSalt(10);
+    let saltPass = await bcrypt.hash(req.body.password, salt);
+
+    newUser.password = saltPass;
+
+    await newUser.save();
+
+    const token = newUser.generateAuthToken();
+
+    let data = pick(newUser, ['_id', 'email', 'role']);
+
+    res.header('x-auth-token', token).send({ ...data, token });
+  },
+  userNormalLogin: async (req, res) => {
+    let { email, password } = req.body;
+
+    let { error } = loginValidator(req.body);
+    if (error) return res.status(400).send({ message: error.message });
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res
+        .status(400)
+        .send({ message: 'کاربری با این اطلاعات یافت نشد' });
+
+    const compare = await bcrypt.compare(password, user.password);
+
+    if (!compare)
+      return res.status(400).send({ message: 'wrong username or password' });
+
+    const token = user.generateAuthToken();
+
+    console.log(user);
+
+    let data = pick(user, [
+      '_id',
+      'name',
+      'email',
+      'countryCode',
+      'phoneNumber',
+      'password',
+      'firstName',
+      'lastName',
+    ]);
+
+    res.header('x-auth-token', token).send({ ...data, token });
+  },
 };
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 const {
   login,
@@ -18,26 +18,30 @@ const {
   userComments,
   sendCodeToUsers,
   getAllUsers,
-} = require("../controllers/user");
-const auth = require("../middlwares/auth");
-const admin = require("../middlwares/admin");
+  userNormalLogin,
+  userNormalRegister,
+} = require('../controllers/user');
+const auth = require('../middlwares/auth');
+const admin = require('../middlwares/admin');
 
-router.post("/api/user/login", login);
-router.post("/api/user/register", register);
-router.post("/api/user/signup", userRegister);
-router.post("/api/user/signin", userLogin);
-router.get("/api/user/me", auth, me);
-router.put("/api/user/:id", auth, update);
-router.post("/api/user/upload/:id", upload);
-router.post("/api/user/otp", sendCode);
-router.post("/api/user/otp-signin", sendCodeToUsers);
-router.post("/api/user/verify", verifyCode);
-router.get("/api/user/comments", auth, userComments);
-router.post("/api/user/address/add/:userId", addAddress);
-router.put("/api/user/address/edit/:addressId/:userId", editAddress);
-router.delete("/api/user/address/remove/:addressId/:userId", removeAddress);
-router.post("/api/user/register-with-phone", registerWithPhoneNumber);
-router.get("/api/stores", getAllStores);
-router.get("/api/users", getAllUsers);
+router.post('/api/user/login', login);
+router.post('/api/user/register', register);
+router.post('/api/user/signup', userRegister);
+router.post('/api/user/signin', userLogin);
+router.get('/api/user/me', auth, me);
+router.put('/api/user/:id', auth, update);
+router.post('/api/user/upload/:id', upload);
+router.post('/api/user/otp', sendCode);
+router.post('/api/user/otp-signin', sendCodeToUsers);
+router.post('/api/user/verify', verifyCode);
+router.get('/api/user/comments', auth, userComments);
+router.post('/api/user/address/add/:userId', addAddress);
+router.put('/api/user/address/edit/:addressId/:userId', editAddress);
+router.delete('/api/user/address/remove/:addressId/:userId', removeAddress);
+router.post('/api/user/register-with-phone', registerWithPhoneNumber);
+router.get('/api/stores', getAllStores);
+router.get('/api/users', getAllUsers);
+router.post('/api/user/user-login', userNormalLogin);
+router.post('/api/user/user-register', userNormalRegister);
 
 module.exports = router;
